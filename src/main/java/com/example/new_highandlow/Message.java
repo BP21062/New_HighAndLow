aabdba68c025e55ece6861e91ee20d7d09f15255
bp21117 <bp21117@shibaura-it.ac.jp>
2023-12-13 16:36:33 +0900

    MessageクラスとMessageContentクラスの実装 また各フィールドの実装

---
+++ src/main/java/com/example/new_highandlow/Message.java
@@ -0,0 +1,7 @@
+package com.example.new_highandlow;
+
+public class Message{
+	private String order;
+	private MessageContent content;
+	private boolean result;
+}
---
+++ src/main/java/com/example/new_highandlow/MessageContent.java
@@ -0,0 +1,9 @@
+package com.example.new_highandlow;
+
+public class MessageContent{
+	private String user_id;
+	private String password;
+	private String score;
+	private String rule;
+	private String error;
+}
