c0c0ffe01487c4764cfa8c7fc553e6270ba06810
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-27 22:26:02 +0900

    MessageContentクラスの再調整

--- src/main/java/com/example/new_highandlow/Message.java
+++ src/main/java/com/example/new_highandlow/Message.java
@@ -1,7 +1,13 @@
 package com.example.new_highandlow;
 
 public class Message{
-	private String order;
-	private MessageContent content;
-	private boolean result;
+	public MessageContent messageContent = new MessageContent();
+	String order;//通信規則用
+	Boolean result;//画面遷移とかのチェック用？
+
+	public Message(String order, String user_id){
+		this.order = order;
+		this.messageContent.user_id = user_id;
+	}
+
 }
