14c6f10441ed528ad204c8059f50178de2914903
watanabe mikio <bp21062@sibaura-it.ac.jp>
2024-01-16 06:43:34 +0900

    マージしたファイル間で発生した齟齬の取り除き ゲームの通信周りを整理

--- src/main/java/com/example/new_highandlow/GameScreen.java
+++ src/main/java/com/example/new_highandlow/GameScreen.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameScreen extends JFrame implements Runnable {
+public class GameScreen extends JFrame{
 	private ArrayList<String> deck_information = new ArrayList<>();
 	private ArrayList<String> card_information = new ArrayList<>();
 	private int trump2_pattern_hearts, trump2_pattern_diamonds, trump2_pattern_spades, trump2_pattern_clubs;
@@ -16,19 +16,18 @@ public class GameScreen extends JFrame implements Runnable {
 	private JLabel remain_time_label, trump2_pattern_label, user_score_label;
 	private JButton high_button, just_button, low_button;
 	private JButton hearts_button, diamonds_button, spades_button, clubs_button;
-	private JLabel HLJChoice, PTChoice;
-	private String user_id;
+	public String HLJChoice,PTNChoice; //各選択肢の内容を保存する関数
 
-	public GameScreen(String user_id) {
+	public GameScreen(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(960, 540);
 		setTitle("Game Screen");
 		setLocationRelativeTo(null);
 		setLayout(null);
 
-		back_ground_panel = new JPanel() {
+		back_ground_panel = new JPanel(){
 			@Override
-			protected void paintComponent(Graphics g) {
+			protected void paintComponent(Graphics g){
 				super.paintComponent(g);
 				ImageIcon image = new ImageIcon("src/main/resources/com/example/new_highandlow/png/Game.png");
 				g.drawImage(image.getImage(), 0, 0, getWidth(), getHeight(), this);
@@ -74,6 +73,7 @@ public class GameScreen extends JFrame implements Runnable {
 		high_button.setFont(new Font("Arial", Font.BOLD, 20));
 		high_button.setBackground(Color.blue);
 		high_button.setForeground(Color.white);
+		high_button.setEnabled(false);
 
 		just_button = new JButton("Just");
 		just_button.setBounds(430, 350, 80, 40);
@@ -82,6 +82,7 @@ public class GameScreen extends JFrame implements Runnable {
 		just_button.setFont(new Font("Arial", Font.BOLD, 20));
 		just_button.setBackground(Color.blue);
 		just_button.setForeground(Color.white);
+		just_button.setEnabled(false);
 
 		low_button = new JButton("Low");
 		low_button.setBounds(560, 350, 80, 40);
@@ -90,6 +91,7 @@ public class GameScreen extends JFrame implements Runnable {
 		low_button.setFont(new Font("Arial", Font.BOLD, 20));
 		low_button.setBackground(Color.blue);
 		low_button.setForeground(Color.white);
+		low_button.setEnabled(false);
 
 		hearts_button = new JButton("♥");
 		hearts_button.setBounds(290, 420, 50, 50);
@@ -98,6 +100,7 @@ public class GameScreen extends JFrame implements Runnable {
 		hearts_button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
 		hearts_button.setBackground(Color.white);
 		hearts_button.setForeground(Color.red);
+		hearts_button.setEnabled(false);
 
 		diamonds_button = new JButton("♦");
 		diamonds_button.setBounds(390, 420, 50, 50);
@@ -106,6 +109,7 @@ public class GameScreen extends JFrame implements Runnable {
 		diamonds_button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
 		diamonds_button.setBackground(Color.white);
 		diamonds_button.setForeground(Color.red);
+		diamonds_button.setEnabled(false);
 
 		spades_button = new JButton("♠");
 		spades_button.setBounds(490, 420, 50, 50);
@@ -114,6 +118,7 @@ public class GameScreen extends JFrame implements Runnable {
 		spades_button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
 		spades_button.setBackground(Color.white);
 		spades_button.setForeground(Color.black);
+		spades_button.setEnabled(false);
 
 		clubs_button = new JButton("♣");
 		clubs_button.setBounds(590, 420, 50, 50);
@@ -122,6 +127,7 @@ public class GameScreen extends JFrame implements Runnable {
 		clubs_button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
 		clubs_button.setBackground(Color.white);
 		clubs_button.setForeground(Color.black);
+		clubs_button.setEnabled(false);
 
 		this.add(back_ground_panel);
 
@@ -139,138 +145,148 @@ public class GameScreen extends JFrame implements Runnable {
 		back_ground_panel.add(clubs_button);
 		setContentPane(back_ground_panel);
 
-		this.user_id = user_id;
-
-		Thread thread = new Thread(this);
-		thread.start();
 	}
 
-	public void displayRemainTime(int time) {
+	public void displayRemainTime(int time){
 		String num = String.format(" 残り時間：%d ", time);
 		remain_time_label.setText(num);
 	}
 
-	public void displaySecondCardInformation(List<Integer> pattern_list) {
-		List<Integer> patterns = new ArrayList<>();
-		for (int a = 0; a < 4; a++) {
-			patterns.set(a, pattern_list.get(a));
-		}
-		String num = String.format("<html><body>&nbsp;2枚目の柄<br />" +
-				"&nbsp;&emsp;♡×" + patterns.get(0) + "<br />" +
-				"&nbsp;&emsp;♢×" + patterns.get(1) + "<br />" +
-				"&nbsp;&emsp;♠×" + patterns.get(2) + "<br />" +
-				"&nbsp;&emsp;♣×" + patterns.get(3) + "<br /></body></html>");
-		trump2_pattern_label.setText(num);
+	public void displayFirstCard(String cardcode){
+		ImageIcon second_card = new ImageIcon(cardcode);
+		trump2_card_label = new JLabel(second_card);
+		CController cController = new CController();
+		cController.finishMessage("displayFirst");
 	}
 
-	public void displayCurrentScore(List<Integer> score_list) {
-		List<Integer> scores = new ArrayList<>();
-		for (int a = 0; a < 4; a++) {
-			scores.set(a, score_list.get(a));
+	public void displaySecondCard(String cardcode){
+		ImageIcon second_card = new ImageIcon(cardcode);
+		trump2_card_label = new JLabel(second_card);
+		try{
+			Thread.sleep(5000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
 		}
-		String num = String.format("<html><body>&nbsp;Player1：" + scores.get(0) + "<br />" +
-				"&nbsp;Player2：" + scores.get(1) + "<br />" +
-				"&nbsp;Player3：" + scores.get(2) + "<br />" +
-				"&nbsp;Player4：" + scores.get(3) + "<br /></body></html>");
-		user_score_label.setText(num);
+		CController cController = new CController();
+		cController.finishMessage("displaySecond");
+	}
+
+	public void displaySecondCardInformation(List<Integer> pattern_list){
+		List<Integer> patterns = new ArrayList<>();
+		// たぶんこれも値をコピーしたかった？
+		for(int a = 0; a < 4; a++){
+			patterns.add(pattern_list.get(a));
+		}
+		trump2_pattern_label.setText("<html><body>&nbsp;2枚目の柄<br />" + "&nbsp;&emsp;♡×" + patterns.get(0) + "<br />" + "&nbsp;&emsp;♢×" + patterns.get(1) + "<br />" + "&nbsp;&emsp;♠×" + patterns.get(2) + "<br />" + "&nbsp;&emsp;♣×" + patterns.get(3) + "<br /></body></html>");
+	}
+
+	public void displayCurrentScore(List<Integer> score_list){
+		List<Integer> scores = new ArrayList<>();
+		// たぶんArrayListをコピーしたかった？
+		for(int a = 0; a < 4; a++){
+			scores.add(score_list.get(a));
+		}
+		user_score_label.setText("<html><body>&nbsp;Player1：" + scores.get(0) + "<br />" + "&nbsp;Player2：" + scores.get(1) + "<br />" + "&nbsp;Player3：" + scores.get(2) + "<br />" + "&nbsp;Player4：" + scores.get(3) + "<br /></body></html>");
+
+		CController cController = new CController();
+		cController.finishMessage("CurrentScore");
 	}
 
 	/*
 	 * public void pushHighButton(ActionEvent event){
 	 * if(event.getSource()==high_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushJustButton(ActionEvent event){
 	 * if(event.getSource()==just_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushLowButton(ActionEvent event){
 	 * if(event.getSource()==low_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushHeartsButton(ActionEvent event){
 	 * if(event.getSource()==hearts_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushDiamondsButton(ActionEvent event){
 	 * if(event.getSource()==diamonds_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushSpadesButton(ActionEvent event){
 	 * if(event.getSource()==spades_button){}
 	 * }
-	 * 
+	 *
 	 * public void pushClubsButton(ActionEvent event){
 	 * if(event.getSource()==clubs_button){}
 	 * }
 	 */
 
-	public void turnUP() {
+	public void turnUP(){
 		spades_button.setEnabled(true);
 		diamonds_button.setEnabled(true);
 		clubs_button.setEnabled(true);
 		hearts_button.setEnabled(true);
 	}
 
-	public void pushButton(ActionEvent event) {
+	public void pushButton(ActionEvent event){
 		String cmd = event.getActionCommand();
-		if (cmd.equals("High")) {
-			System.out.println("h");
+		if(cmd.equals("High")){
+			HLJChoice = "high";
 			turnUP();
-		} else if (cmd.equals("Low")) {
-			System.out.println("l");
+		}else if(cmd.equals("Low")){
+			HLJChoice = "l";
 			turnUP();
-		} else if (cmd.equals("Just")) {
-			System.out.println("j");
+		}else if(cmd.equals("Just")){
+			HLJChoice = "j";
 			turnUP();
 		}
 	}
 
-	public void pushPatternButton(ActionEvent event) {
+	public void pushPatternButton(ActionEvent event){
 		String cmd = event.getActionCommand();
-		if (cmd.equals("heart")) {
-			System.out.println("he");
-		} else if (cmd.equals("dia")) {
-			System.out.println("d");
-		} else if (cmd.equals("club")) {
-			System.out.println("c");
-		} else if (cmd.equals("spade")) {
-			System.out.println("s");
+		if(cmd.equals("heart")){
+			PTNChoice = "heart";
+		}else if(cmd.equals("dia")){
+			PTNChoice = "diamond";
+		}else if(cmd.equals("club")){
+			PTNChoice = "club";
+		}else if(cmd.equals("spade")){
+			PTNChoice = "spade";
 		}
 	}
 
-	@Override
-	public void run() {
-		for (int i = 0; i < 5; i++) {
-			high_button.setEnabled(false);
-			just_button.setEnabled(false);
-			low_button.setEnabled(false);
-			spades_button.setEnabled(false);
-			diamonds_button.setEnabled(false);
-			clubs_button.setEnabled(false);
-			hearts_button.setEnabled(false);
-			try {
+	public void startGameTimer(){
+		high_button.setEnabled(true);
+		just_button.setEnabled(true);
+		low_button.setEnabled(true);
+
+		for(int j = 0; j < 20; j++){
+			displayRemainTime(20 - j);
+			try{
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
+			}catch(InterruptedException e){
 				e.printStackTrace();
 			}
-			high_button.setEnabled(true);
-			just_button.setEnabled(true);
-			low_button.setEnabled(true);
-
-			for (int j = 0; j < 5; j++) {
-				displayRemainTime(5 - j);
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			displayRemainTime(0);
 		}
+		displayRemainTime(0);
+		high_button.setEnabled(false);
+		just_button.setEnabled(false);
+		low_button.setEnabled(false);
+		hearts_button.setEnabled(false);
+		clubs_button.setEnabled(false);
+		diamonds_button.setEnabled(false);
+		spades_button.setEnabled(false);
 
-		SController sController = new SController(user_id);
-		sController.changeScreen("Result");
-		this.setVisible(false);
+		CController cController = new CController();
+		cController.finishMessage("Timer");
+
 	}
+
+	public void changeScreen(String screen) {
+		this.setVisible(false);
+		SController.changeScreen(screen);
+
+	}
+
 }
--- src/main/java/com/example/new_highandlow/SController.java
+++ src/main/java/com/example/new_highandlow/SController.java
@@ -2,50 +2,74 @@ package com.example.new_highandlow;
 
 import javax.swing.*;
 
-public class SController{
+public class SController {
 
-	static String user_id;
-	int room_id;
-	Message message;
+	// staticにすることで共通化
+	static int room_id;
+	static Message message;
 
-	SController(String user_id){
-		SController.user_id = user_id;
+	// GameScreenは毎回作り直す
+	// 他は使いまわす
+
+	// オーバーロードする
+	public static void changeScreen(String screen_id, Message message) {
+		if (screen_id.equals("Wait") || screen_id.equals("Score") || screen_id.equals("Result") || screen_id.equals("Rule")) {
+			SController.message = message;
+		} else {
+			System.out.println("[client] Error: changeScreen()");
+		}
+
+		// websocketから来た画像
+		if (screen_id.equals("Rule")) {
+			CController.ruleScreen = new RuleScreen(message);
+		}
+
+		changeScreen(screen_id);
 	}
 
-	public void changeScreen(String screen_id){
-			JFrame newScreen = null;
+	// 画面遷移用
+	public static void changeScreen(String screen_id) {
+		JFrame newScreen = null;
 
-			switch(screen_id){
-				case "Lobby":
-					LobbyScreen lobbyScreen = new LobbyScreen();
-					newScreen = lobbyScreen;
-					CController.lobbyScreen = lobbyScreen;
-					break;
-				case "Start":
-					StartScreen startScreen = new StartScreen(user_id);
-					newScreen = startScreen;
-					CController.startScreen = startScreen;
-					break;
-				case "Score":
-					ScoreScreen scoreScreen = new ScoreScreen(message,user_id);
-					newScreen = scoreScreen;
-					break;
-				case "Rule":
-					newScreen = new RuleScreen(message,user_id);
-					break;
-				case "Wait":
-					newScreen = new WaitScreen(user_id,room_id);
-					break;
-				case "Game":
-					newScreen = new GameScreen(user_id);
-					break;
-				case "Result":
-					newScreen = new ResultScreen(user_id);
-					break;
-			}
+		// 使いまわせるなら使いまわした方がいい
+		switch (screen_id) {
+			case "Lobby":
+				// これはCControllerの起動時に作成される
+				newScreen = CController.lobbyScreen;
+				break;
+			case "Start":
+				// これはCControllerの起動時に作成される
+				newScreen = CController.startScreen;
+				break;
+			case "Score":
+				// これはCControllerの起動時に作成される
+				// 再利用
+				CController.scoreScreen.reload(message);
+				newScreen = CController.scoreScreen;
+				break;
+			case "Rule":
+				// websocketから来たら作成される
+				newScreen = CController.ruleScreen;
+				break;
+			case "Wait":
+				// これはCControllerの起動時に作成される
+				// 再利用
+				CController.waitScreen.reload(SController.room_id);
+				newScreen = CController.waitScreen;
+				break;
+			case "Game":
+				// websocketから来たら作成される
+				newScreen = CController.gameScreen;
+				break;
+			case "Result":
+				// これはCControllerの起動時に作成される
+				// 再利用
+				newScreen = CController.resultScreen;
+				break;
+		}
 
-			if (newScreen != null) {
-				newScreen.setVisible(true);
-			}
+		if (newScreen != null) {
+			newScreen.setVisible(true);
+		}
 	}
 }
