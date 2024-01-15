9a3c6b128a20a873e6e1566b849f92d4b1118768
watanabe mikio <bp21062@sibaura-it.ac.jp>
2024-01-16 06:56:56 +0900

    Re:マージしたファイル間で発生した齟齬の取り除き・ゲームの通信周りを整理

--- src/main/java/com/example/new_highandlow/CController.java
+++ src/main/java/com/example/new_highandlow/CController.java
@@ -5,159 +5,204 @@ import javax.swing.SwingUtilities;
 import com.google.gson.Gson;
 
 //WebSocketClientSample.java
-public class CController{
-	public static String User_id;
+public class CController {
+	public static String user_id;
 	public static String passwd;
 
-	static CServerConnector lobby_connect;
-	static CServerConnector app_connect;
+	private static CServerConnector lobby_connect;
+	private static CServerConnector app_connect;
+
+	// 基本的にこれをあちこちで使いまわす
+	// 例外はWaitScreen, ScoreScreen, GameScreen, ResultScreen
 	static LobbyScreen lobbyScreen;
 	static StartScreen startScreen;
 	static WaitScreen waitScreen;
 	static GameScreen gameScreen;
 	static ResultScreen resultScreen;
+	static RuleScreen ruleScreen;
+	static ScoreScreen scoreScreen;
 	/*
-	 *  サーバ側のエンドポイントと合わせる．2箇所確認する．
-	 *  1. mainメソッド内でserverインスタンスを生成する際のContextRoot
-	 *     サンプル例）static String contextRoot = "/app";
-	 *  2. エンドポイントクラスのアノテーション（@ServerEndpoint("エンドポイント名")）
-	 *     サンプル例）@ServerEndpoint("/sample")
-	 *  この場合指定すべきエンドポイントは上記2つとサーバのアドレス、ポート番号、プロトコルから決定され、
-	 *  例えば、"ws://localhost:8080/app/sample"といった形になる。
-	 *  この例ではプロトコルはWebSocket（ws），ポートは8080，サーバアドレスはlocalhost
-	 *  使い分けるときは適宜Stringとして分割して定義し結合すれば良い．
+	 * サーバ側のエンドポイントと合わせる．2箇所確認する．
+	 * 1. mainメソッド内でserverインスタンスを生成する際のContextRoot
+	 * サンプル例）static String contextRoot = "/app";
+	 * 2. エンドポイントクラスのアノテーション（@ServerEndpoint("エンドポイント名")）
+	 * サンプル例）@ServerEndpoint("/sample")
+	 * この場合指定すべきエンドポイントは上記2つとサーバのアドレス、ポート番号、プロトコルから決定され、
+	 * 例えば、"ws://localhost:8080/app/sample"といった形になる。
+	 * この例ではプロトコルはWebSocket（ws），ポートは8080，サーバアドレスはlocalhost
+	 * 使い分けるときは適宜Stringとして分割して定義し結合すれば良い．
 	 */
-	static String serverAppEndpoint = "ws://localhost:8082/app/playgame"; //appサーバー用
+	static String serverAppEndpoint = "ws://localhost:8082/app/playgame"; // appサーバー用
 	static String serverLobbyEndpoint = "ws://localhost:8080/lobby/";
-
 
 	static int id = 0;
 	static String password = "password";
 	static Gson gson = new Gson();
 
-
-	public static boolean checkPasswordStrength(){
-		return passwd.matches("(?=.*[A-Z]).{8,12}"); //パスワードが条件を満たしていればtrue　それ以外はfalse
+	public static boolean checkPasswordStrength() {
+		return passwd.matches("(?=.*[A-Z]).{8,12}"); // パスワードが条件を満たしていればtrue それ以外はfalse
 	}
 
-	public static void registerUser(){
-		// 試しにSampleMessageのインスタンスを作ってみる
-		Message sendMessage = new Message("2", User_id);
-		// クラスオブジェクトをString (JSON) に変換する
+	public static void registerUser() {
+
+		Message sendMessage = new Message("2", user_id);
+
 		sendMessage.messageContent.password = passwd;
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
+
+		// ログ
+		System.out.println("[client]: registerUser(): " + sendMessageJson + "\n");
+
 		lobby_connect = new CServerConnector(serverLobbyEndpoint);
-		if(lobby_connect.connect()){
+		if (lobby_connect.connect()) {
 			lobby_connect.sendMessage(sendMessageJson);
-		}else{
+		} else {
 			lobbyScreen.displayMessage("※接続に失敗しました");
 		}
 	}
 
-	public void login(){
-		System.out.println("sendMessage()");
-		// 試しにSampleMessageのインスタンスを作ってみる
-		Message sendMessage = new Message("4", User_id);
-		// クラスオブジェクトをString (JSON) に変換する
+	public static void login() {
+
+		Message sendMessage = new Message("4", user_id);
+
 		sendMessage.messageContent.password = passwd;
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
+
+		// ログ
+		System.out.println("[client]: login(): " + sendMessageJson + "\n");
+
 		lobby_connect = new CServerConnector(serverLobbyEndpoint);
-		if(lobby_connect.connect()){
+		if (lobby_connect.connect()) {
 			lobby_connect.sendMessage(sendMessageJson);
-		}else{
+		} else {
 			lobbyScreen.displayMessage("※接続に失敗しました");
 		}
 	}
 
-	public void logout(String user_id){
-		System.out.println("sendMessage()");
-		// 試しにSampleMessageのインスタンスを作ってみる
-		Message sendMessage = new Message("7", user_id);
-		// クラスオブジェクトをString (JSON) に変換する
-		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
+	public static void logout() {
+
+		// 結局メッセージは送らずdisconnect()だけ
+
 		lobby_connect.disconnect();
+		if (app_connect != null) {
+			app_connect.disconnect();
+		}
+
+		// ログ
+		System.out.println("[client]: logout(): Done");
+
 	}
 
+	public static void getScore() {
 
-	public void getScore(String user){
-		System.out.println("sendMessage()");
-		// 試しにSampleMessageのインスタンスを作ってみる
-		Message sendMessage = new Message("1000", user);
-		// クラスオブジェクトをString (JSON) に変換する
+		Message sendMessage = new Message("8", user_id);
+
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
+
+		// ログ
+		System.out.println("[client]: getScore(): " + sendMessageJson + "\n");
+
 		lobby_connect.sendMessage(sendMessageJson);
 	}
 
-	public void getRule(){
-		System.out.println("戦績を表示");
-		// 試しにSampleMessageのインスタンスを作ってみる
-		Message sendMessage = new Message("1001", User_id);
-		// クラスオブジェクトをString (JSON) に変換する
+	public static void getRule() {
+
+		Message sendMessage = new Message("9", user_id);
+
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
+
+		// ログ
+		System.out.println("[client]: getRule(): " + sendMessageJson + "\n");
+
 		lobby_connect.sendMessage(sendMessageJson);
 	}
 
-	public void enter(String user_id, int room_id){
-		SController sc = new SController(user_id);
-		sc.Room_id = room_id;
-		sc.User_id = user_id;
-		System.out.println("sendMessage()");
-		// 試しにSampleMessageのインスタンスを作ってみる
+	public static void enter(int room_id) {
+		SController.room_id = room_id;
+
 		Message sendMessage = new Message("1003", user_id);
 		sendMessage.messageContent.room_id = room_id;
-		// クラスオブジェクトをString (JSON) に変換する
+
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。（JSON）
-		System.out.println(sendMessageJson);
+
+		// ログ
+		System.out.println("[client]: enter(): " + sendMessageJson + "\n");
+
+		// Appサーバーへの接続の確率
 		app_connect = new CServerConnector(serverAppEndpoint);
-		if(app_connect.connect()){
+		if (app_connect.connect()) {
 			app_connect.sendMessage(sendMessageJson);
 			startScreen.setVisible(false);
-			waitScreen = new WaitScreen(User_id, room_id);
+			waitScreen = new WaitScreen(room_id);
 			waitScreen.setVisible(true);
-			waitScreen.displayRoomNumber(room_id);
-		}else{
+		} else {
 			startScreen.displayMessage("※接続に失敗しました");
 		}
 	}
 
-	public boolean checkRoomState(String user_id, int room_id){
-		SController sc = new SController(user_id);
-		sc.Room_id = room_id;
-		sc.User_id = user_id;
-		System.out.println("sendMessage()");
-		// 試しにSampleMessageのインスタンスを作ってみる
+	public static boolean checkRoomState(String user_id, int room_id) {
+		SController.room_id = room_id;
+
 		Message sendMessage = new Message("6", user_id);
-		// クラスオブジェクトをString (JSON) に変換する
+
 		sendMessage.messageContent.room_id = room_id;
 		String sendMessageJson = gson.toJson(sendMessage);
-		// 変換後の書式を表示してみる。JSON
-		System.out.println(sendMessageJson);
-		if(lobby_connect.connect()){
+
+		// ログ
+		System.out.println("[client]: checkRoomState(): " + sendMessageJson + "\n");
+
+		if (lobby_connect.isConnected()) {
 			lobby_connect.sendMessage(sendMessageJson);
 			return true;
-		}else{
+		} else
 			startScreen.displayMessage("※接続に失敗しました");
-			return false;
+		return false;
+	}
+
+	public void finishMessage(String finishTask){
+		if(finishTask.equals("CurrentScore")){
+			Message message = new Message("1005",user_id);
+			message.result = true;
+			String sendMessageJson = gson.toJson(message);
+			app_connect.sendMessage(sendMessageJson);
+		}else if(finishTask.equals("displayFirst")){
+			Message message = new Message("1006",user_id);
+			message.result = true;
+			String sendMessageJson = gson.toJson(message);
+			app_connect.sendMessage(sendMessageJson);
+			gameScreen.startGameTimer();
+		}else if(finishTask.equals("Timer")){
+			Message message = new Message("1007",user_id);
+			message.messageContent.choice = gameScreen.HLJChoice;
+			message.messageContent.pattern = gameScreen.PTNChoice;
+			String sendMessageJson = gson.toJson(message);
+			app_connect.sendMessage(sendMessageJson);
+		}else if(finishTask.equals("displaySecond")){
+			Message message = new Message("1008",user_id);
+			message.result = true;
+			String sendMessageJson = gson.toJson(message);
+			app_connect.sendMessage(sendMessageJson);
 		}
+
+
 	}
 
 	public static void main(String[] args) {
 
 		SwingUtilities.invokeLater(() -> {
+
+			// ロビー画面の作成
 			LobbyScreen lobbyScreen = new LobbyScreen();
-			CController.lobbyScreen = lobbyScreen;
 			lobbyScreen.setVisible(true);
+			CController.lobbyScreen = lobbyScreen;
+
+			CController.startScreen = new StartScreen();
+			// CController.ruleScreen = new RuleScreen();	// 画像があるため作成しない
+			CController.scoreScreen = new ScoreScreen(null);
+			// CController.gameScreen = new GameScreen(user_id);	// 再利用が難しいためなし
+			CController.resultScreen = new ResultScreen();
+			CController.waitScreen = new WaitScreen(-1);
+
 		});
 	}
 }
--- src/main/java/com/example/new_highandlow/StartScreen.java
+++ src/main/java/com/example/new_highandlow/StartScreen.java
@@ -5,19 +5,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
-public class StartScreen extends JFrame{
-	private JButton room1_button,room2_button,room3_button,room4_button,room5_button,room6_button,logout_button,score_button,rule_button;
+public class StartScreen extends JFrame {
+	private JButton room1_button, room2_button, room3_button, room4_button, room5_button, room6_button, logout_button,
+			score_button, rule_button;
 	private JPanel back_ground_panel;
-	private JLabel title_label,message_label;
-	private ArrayList<Button> enter_room_button_list;  //不要っぽそう
-	private ArrayList<String> numberofpeople_list;	//各部屋に入っている人数を格納するリスト
-	//要求仕様書のように部屋に入っている人数を表示するなら、画面遷移の時にアプリケーションサーバから引っ張ってくる必要がある
-	private String user_id;  //pushLogoutButton()で必要だが、現時点で未定義
-	//「addUserid(user_id)」のようなメソッドを定義して、LobbyScreenから遷移する時に引っ張ってくるのが簡単そう
-	public static boolean room_state_flag; //部屋の入室可否変数
+	private JLabel title_label, message_label;
+	private ArrayList<JButton> enter_room_button_list; // 不要っぽそう
+	private ArrayList<String> numberofpeople_list; // 各部屋に入っている人数を格納するリスト
+	// 要求仕様書のように部屋に入っている人数を表示するなら、画面遷移の時にアプリケーションサーバから引っ張ってくる必要がある
+	private String user_id; // pushLogoutButton()で必要だが、現時点で未定義
+	// 「addUserid(user_id)」のようなメソッドを定義して、LobbyScreenから遷移する時に引っ張ってくるのが簡単そう
+	public static Boolean room_state_flag; // 部屋の入室可否変数
 
-	public StartScreen(String user_id){
-		this.user_id=user_id;
+	public StartScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(960, 540);
 		setTitle("Start Screen");
@@ -28,13 +28,12 @@ public class StartScreen extends JFrame{
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				ImageIcon image = new ImageIcon("src/main/resources/com/example/new_highandlow/png/Game.png");	//背景画像は仮としてGame画面の物を使用
+				ImageIcon image = new ImageIcon("src/main/resources/com/example/new_highandlow/png/Game.png"); // 背景画像は仮としてGame画面の物を使用
 				g.drawImage(image.getImage(), 0, 0, getWidth(), getHeight(), this);
 			}
 		};
 		back_ground_panel.setLayout(null);
 		back_ground_panel.setOpaque(false);
-
 
 		title_label = new JLabel("High&Low&Just");
 		title_label.setBounds(360, 40, 300, 40);
@@ -121,185 +120,94 @@ public class StartScreen extends JFrame{
 		back_ground_panel.add(rule_button);
 
 		setContentPane(back_ground_panel);
-		this.setVisible(true);
 	}
 
-
-	public void pushLogoutButton(ActionEvent event){
-		if(event.getSource()== logout_button){
-			CController cController=new CController();
-			cController.logout(this.user_id);
-			Message message=null;
-			changeScreen("Lobby",message);
+	public void pushLogoutButton(ActionEvent event) {
+		if (event.getSource() == logout_button) {
+			CController.logout();
+			changeScreen("Lobby");
 		}
 	}
 
-	public void pushScoreButton(ActionEvent event){
-		if(event.getSource()== score_button){
-			CController cController=new CController();
-			cController.getScore(user_id);
-			//changeScreen("Score");
+	public void pushScoreButton(ActionEvent event) {
+		if (event.getSource() == score_button) {
+			CController.getScore();
+			// changeScreen("Score");
 		}
 	}
 
-	public void pushRuleButton(ActionEvent event){
-		if(event.getSource()== rule_button){
-			CController cController=new CController();
-			cController.getRule();
-			//changeScreen("Rule");
+	public void pushRuleButton(ActionEvent event) {
+		if (event.getSource() == rule_button) {
+			if (CController.ruleScreen == null) {
+				CController.getRule();
+			}
+			
+			changeScreen("Rule");
 		}
 	}
 
-	public void pushEnterRoomButton(ActionEvent event){
-		if(event.getSource()== room1_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 1)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
+	public void pushEnterRoomButton(ActionEvent event) {
+
+		int room_id = 0;
+
+		if (event.getSource() == room1_button) {
+			room_id = 1;
+		} else if (event.getSource() == room2_button) {
+			room_id = 2;
+		} else if (event.getSource() == room3_button) {
+			room_id = 3;
+		} else if (event.getSource() == room4_button) {
+			room_id = 4;
+		} else if (event.getSource() == room5_button) {
+			room_id = 5;
+		} else if (event.getSource() == room6_button) {
+			room_id = 6;
+		} else {
+			System.out.println("[client] Error: pushEnterRoomButton()");
+		}
+
+		// まずnullを入れる
+		room_state_flag = null;
+
+		// これは接続できたか否か
+		if (CController.checkRoomState(user_id, room_id)) {
+			/*
+			 * checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
+			 * 次の処理を開始してしまう非同期処理になる?
+			 * ⇒便宜的にウェイトを置いて対処
+			 */
+
+
+			// 書き換えが来るとnullではなくなる
+			while (room_state_flag == null){
 				try {
-					Thread.sleep(3000);
+					Thread.sleep(500);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 1);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 1;
-					//sController.changeScreen("Wait");
-					this.setVisible(false);
 				}
 			}
-		}else if(event.getSource() == room2_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 2)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 2);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 2;
-					sController.changeScreen("Wait");
-					this.setVisible(false);
-				}
-			}
-		}else if(event.getSource() == room3_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 3)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 3);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 3;
-					sController.changeScreen("Wait");
-					this.setVisible(false);
-				}
-			}
-		}else if(event.getSource() == room4_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 4)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 4);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 4;
-					sController.changeScreen("Wait");
-					this.setVisible(false);
-				}
-			}
-		}else if(event.getSource() == room5_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 5)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 5);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 5;
-					sController.changeScreen("Wait");
-					this.setVisible(false);
-				}
-			}
-		}else if(event.getSource() == room6_button){
-			room_state_flag = false;
-			CController cController=new CController();
-			if(cController.checkRoomState(user_id, 6)){
-				/*
-					checkRoomStateメソッドの処理終了(=入室可否が返信される)を待たずに
-					次の処理を開始してしまう非同期処理になる?
-					⇒便宜的にウェイトを置いて対処
-				*/
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if(room_state_flag){
-					cController.enter(user_id, 6);
-					SController sController = new SController(user_id);
-					sController.User_id = user_id;
-					sController.Room_id = 6;
-					sController.changeScreen("Wait");
-					this.setVisible(false);
-				}
+			if (room_state_flag) {
+				CController.enter(room_id);
+				SController.room_id = room_id;
+
+				this.setVisible(false);
+				changeScreen("Wait");
 			}
 		}
+
 	}
 
-	public void changeScreen(String screen,Message message){
-		SController sc = new SController(user_id);
-		sc.message=message;
-		sc.changeScreen(screen);
+	public void changeScreen(String screen) {
 		this.setVisible(false);
+		SController.changeScreen(screen);
+		
+	}
+	public void changeScreen(String screen, Message message) {
+		this.setVisible(false);
+		SController.changeScreen(screen, message);
 	}
 
-	public void displayMessage(String displayString){
+	public void displayMessage(String displayString) {
 		message_label.setText(displayString);
 	}
 }
